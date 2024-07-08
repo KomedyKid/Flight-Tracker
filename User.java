e9971e0282ae1a0605204565801e96f96a208bc5
KomedyKid <salim.yahyaei@gmail.com>
2024-07-09 00:02:44 +0400

    made user class

---
+++ User.java
@@ -0,0 +1,33 @@
+public class User {
+
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasssword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String toString() {
+
+        return "Username: "+username+"\n"+"Password: "+password;
+
+    }
+    
+}
