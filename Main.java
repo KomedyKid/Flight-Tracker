ef329f4fd549e2ae315a42770026d00937c9b7f6
KomedyKid <salim.yahyaei@gmail.com>
2024-07-09 00:06:41 +0400

    testing the user class and tostring

--- Main.java
+++ Main.java
@@ -12,6 +12,13 @@ public class Main {
 
         System.out.println(flight_1);
 
+        String username = "ABC123";
+        String password = "1234";
+
+        User user1 = new User(username, password);
+
+        System.out.println(user1);
+
 
     }
     
