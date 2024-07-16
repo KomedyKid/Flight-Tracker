1540ca73477cf0a91e83bbdf7d3eea894c45aac5
KomedyKid <72435833+KomedyKid@users.noreply.github.com>
2024-07-16 18:11:51 +0400

    Integrated Flighttracking URL class into Flight class to make flight objects with the tracking URL

--- Flight.java
+++ Flight.java
@@ -4,11 +4,13 @@ public class Flight{
     private String flightCode;
     private LocalDateTime departureTime;
     private LocalDateTime arrivalTime; 
+    private FlightTrackingURL trackingURL;
 
     public Flight(String flightCode, LocalDateTime departureTime, LocalDateTime arrivalTime) {
         this.flightCode = flightCode;
         this.departureTime = departureTime;
         this.arrivalTime = arrivalTime;
+        this.trackingURL = new FlightTrackingURL(flightCode);
     }
 
     public String getFlightCode() {
@@ -35,8 +37,12 @@ public class Flight{
         this.arrivalTime = arrivalTime;
     }
 
+    public string getTrackingURL() {
+        return new FlightTrackingURL(flightCode);
+    }
+
     public String toString() {
         
-        return "Flightcode: " + flightCode + "\n"+"Departure Time: "+departureTime+"\n"+"Arrival Time: "+arrivalTime;
+        return "Flightcode: " + flightCode + "\n"+"Departure Time: "+departureTime+"\n"+"Arrival Time: "+arrivalTime+"\n"+"Tracking URL: "+trackingURL;
     }
 }
--- FlightTrackingURL.java
+++ FlightTrackingURL.java
@@ -5,4 +5,8 @@ public class FlightTrackingURL {
         this.flightTrackingURL = "https://www.flightradar24.com/"+flightcode;
     }
     
+    public String toString() {
+        
+        return flightTrackingURL;
+    }
 }
